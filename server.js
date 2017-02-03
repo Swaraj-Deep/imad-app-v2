85b7294c1da59313caf4017ae021b09df6256d5a
Swaraj Deep <swarajdeep33445@gmail.com>
2017-02-03 13:30:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,20 +4,54 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var articleOne = {
-    title :'Article One|Swaraj Deep',
-    heading:'Article One',
-    date: '2 feb 2017',
-    content:`
-                 <p>
-                    Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.
-                </p>
-                <p>
-                    Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.
-                </p>
-                <p>
-                    Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.
-                </p>`
+var articles=
+{
+    'article-one ': {
+        title :'Article One|Swaraj Deep',
+        heading:'Article One',
+        date: '2 feb 2017',
+        content:`
+                     <p>
+                        Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.
+                    </p>
+                    <p>
+                        Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.
+                    </p>
+                    <p>
+                        Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.Hi! I am Swaraj Deep.This is my very first page on my webapp.
+                    </p>`
+    },
+    'article-two':{
+        title :'Article Two|Swaraj Deep',
+        heading:'Article Two',
+        date: '3 feb 2017',
+        content:`
+                     <p>
+                        Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.
+                    </p>
+                    <p>
+                        Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.
+                    </p>
+                    <p>
+                        Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.Hi! I am Swaraj Deep.This is my very second page on my webapp.
+                    </p>
+                    `},
+    'article-three':{
+        title :'Article Three|Swaraj Deep',
+        heading:'Article Three',
+        date: '6 feb 2017',
+        content:`
+                     <p>
+                        Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.
+                     </p>
+                     <p>
+                        Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.
+                     </p>
+                     <p>
+                        Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.   Hi! I am Swaraj Deep.This is my third page on my webapp.
+                     </p>
+                     `
+        },
 };
 function createTemplate(data)
 {
@@ -64,15 +98,11 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/article-one',function(req,res){
-   res.send(createTemplate(articleOne));
+app.get('/:articleName',function(req,res){
+    var articleName=rq.params.articleName;
+   res.send(createTemplate(articles[artiicleName]));
 });
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-two.html'));
-});
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-three.html'));
-});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
