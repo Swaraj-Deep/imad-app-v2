3db7e71ca6d184553cfc2e420cc6cda36fd1c834
Swaraj Deep <swarajdeep33445@gmail.com>
2017-02-03 13:34:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
-   res.send(createTemplate(articles[artiicleName]));
+   res.send(createTemplate(articles[articleName]));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
