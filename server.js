1f030188e9aa947c4b30390b9941bd5c1e6e8d8d
Swaraj Deep <swarajdeep33445@gmail.com>
2017-02-07 08:45:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,6 +96,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
