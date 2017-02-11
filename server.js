d09bdb204f7b0bee22e200cf3aca7d91efb963db
Swaraj Deep <swarajdeep33445@gmail.com>
2017-02-11 11:10:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var names=[];
+app.get('/submit-name',function(req,res){
+    
+   var name=req.query.name;
+   names.push(name);
+   res.send(JSON.stringify(names));
+    
+});
 var articles=
 {
     'article-one':{
@@ -116,14 +124,7 @@ app.get('/:articleName',function(req,res){
    res.send(createTemplate(articles[articleName]));
 });
 
-var names=[];
-app.get('/submit-name',function(req,res){
-    
-   var name=req.query.name;
-   names.push(name);
-   res.send(JSON.stringify(names));
-    
-});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
