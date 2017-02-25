ac52a56e87fe9bed1a4d8660dd9b0bc5825467c6
Swaraj Deep <swarajdeep33445@gmail.com>
2017-02-25 11:01:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var pool =require('pg').pool;
 var app = express();
 app.use(morgan('combined'));
+var config ={
+    user:'swaraj deep' ,
+    database:'swaraj deep',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+}
 var names=[];
 app.get('/submit-name',function(req,res){// /submit-name?name=xxxx
     
@@ -103,6 +110,30 @@ function createTemplate(data)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool=new pool(config);
+
+app.get('/test-db',function(req,res){
+    
+   //make a select request
+   // return a response with the results.
+   pool.query('SELECT * FROM test',function(err,result){
+      if(err) {
+          
+          res.status(500).send(err.toString());
+      }else{
+          
+          res.send(JSON.stringify(result));
+      }
+       
+   });
+   
+   
+});
+
+
+
+
 
 var counter=0;
 app.get('/counter',function(req,res){
