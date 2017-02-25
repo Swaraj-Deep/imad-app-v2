c5dbda27db492b0d9e1b451d9300a777d962c4ba
Swaraj Deep <swarajdeep33445@gmail.com>
2017-02-25 11:22:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool =require('pg').Pool;
 var config ={
-    user:'swaraj deep' ,
-    database:'swaraj deep',
+    user:'swaraj-deep' ,
+    database:'swaraj-deep',
     host:'db.imad.hasura-app.io',
     port:'5432',
     password: process.env.DB_PASSWORD
