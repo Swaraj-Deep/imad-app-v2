0723132a438522e6528b77a4844db424d7bf0017
Swaraj Deep <swarajdeep33445@gmail.com>
2017-02-25 11:13:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,24 @@ function createTemplate(data)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var pool = new Pool(config);
 
+app.get('/test-db',function(req,res){
+    
+   //make a select request
+   // return a response with the results.
+   pool.query('SELECT * FROM test',function(err,result){
+      if(err) {
+          
+          res.status(500).send(err.toString());
+      }else{
+          
+          res.send(JSON.stringify(result.rows));
+      }
+       
+   });
+   
+});
 var counter=0;
 app.get('/counter',function(req,res){
    counter=counter+1;
