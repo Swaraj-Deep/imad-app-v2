a5a6180497caa2ba1d443ec335f153c3fa2834f4
Swaraj Deep <swarajdeep33445@gmail.com>
2017-02-25 11:09:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool =require('pg').pool;
+var Pool =require('pg').Pool;
 var config ={
     user:'swaraj deep' ,
     database:'swaraj deep',
@@ -112,7 +112,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new pool(config);
+var pool = new Pool(config);
 
 app.get('/test-db',function(req,res){
     
