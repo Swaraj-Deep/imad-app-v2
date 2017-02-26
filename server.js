468793e5a91eb642bc8f6f97bacd4a9ab50bd99c
Swaraj Deep <swarajdeep33445@gmail.com>
2017-02-26 13:00:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/articles/:articleName',function(req,res){
-    var articleName=req.params.articleName;
+   
     pool.query("SELECT * FROM  article WHERE title = '" + req.params.articleName + "'",function(err,result){
         if(err) {
             
