c5758576013f35cb5c1231ee9bc5c9c66dec01d5
Swaraj Deep <swarajdeep33445@gmail.com>
2017-02-26 12:51:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,9 +144,26 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/:articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
     var articleName=req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+    pool.query("SELECT * FROM  article WHERE title =" + req.params.articleName,function(err,result){
+        if(err) {
+            
+            res.status(500).send(err.toString());
+        }else{
+            
+            if(result.rows.length===0){
+                
+                res.status(404).send('We were unable to find the article requested');
+            }
+            else{
+                
+                var articleData=result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+   
 });
 
 
